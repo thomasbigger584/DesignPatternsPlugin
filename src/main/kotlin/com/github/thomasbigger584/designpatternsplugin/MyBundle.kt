5d7abbe43e291f99a4c0df48d8ecc11c76f9a2c5
GitHub Action <action@github.com>
2021-10-06 19:39:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/thomasbigger584/designpatternsplugin/MyBundle.kt
+++ src/main/kotlin/com/github/thomasbigger584/designpatternsplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.thomasbigger584.designpatternsplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/thomasbigger584/designpatternsplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/thomasbigger584/designpatternsplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.thomasbigger584.designpatternsplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.thomasbigger584.designpatternsplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
